8115c71d7dfbea10a3943c8637625f4cd24b8606
liveangela <cs198500@126.com>
2018-08-12 23:20:50 +0800

    [^]Frontend: use bottom-nav

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,41 @@
 <template lang="pug">
   v-app
-    v-navigation-drawer(
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    )
-      v-list
-        v-list-tile(
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        )
-            v-list-tile-action
-              v-icon(v-html="item.icon")
-            v-list-tile-content
-              v-list-tile-title(v-text="item.title")
-    v-toolbar(app :clipped-left="clipped")
-      v-toolbar-side-icon(@click.stop="drawer = !drawer")
-      v-btn(icon @click.stop="miniVariant = !miniVariant")
-        v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
-      v-btn(icon @click.stop="clipped = !clipped")
-        v-icon web
-      v-btn(icon @click.stop="fixed = !fixed")
-        v-icon remove
-      v-toolbar-title(v-text="title")
-      v-spacer
-      v-btn(icon @click.stop="rightDrawer = !rightDrawer")
-        v-icon menu
     v-content
-      HelloWorld
-    v-navigation-drawer(
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
+      v-container(fluid)
+        router-view
+    v-bottom-nav(
+      :active.sync="bottomNav"
+      :color="color"
+      :value="true"
+      absolute
       app
     )
-      v-list
-        v-list-tile(@click="right = !right")
-          v-list-tile-action
-            v-icon compare_arrows
-          v-list-tile-title Switch drawer (click me)
-    v-footer(:fixed="fixed" app)
-      span &copy; 2018
+      v-btn(
+        v-for="(list, i) in bottomList"
+        :key="i"
+        dark
+      )
+        span {{list.text}}
+        v-icon {{list.icon}}
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class App extends Vue {
-  private clipped = false
-  private drawer = true
-  private fixed = false
-  private items = [{
-    icon: 'bubble_chart',
-    title: 'Inspire',
-  }]
-  private miniVariant = false
-  private right = true
-  private rightDrawer = false
-  private title = 'Title.js'
+  private bottomNav = 0
+  private bottomList = [
+    {text: 'Video', icon: 'ondemand_video', color: 'blue-grey'},
+    {text: 'Music', icon: 'music_note', color: 'teal'},
+    {text: 'Book', icon: 'book', color: 'brown'},
+    {text: 'Image', icon: 'image', color: 'indigo'},
+    {text: 'Web', icon: 'web', color: 'deep-purple'},
+  ]
+
+  get color() {
+    const {bottomNav, bottomList} = this
+    return bottomList[bottomNav].color
+  }
 }
 </script>
