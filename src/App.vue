766336af583b0d29d732722bd5c1402493eba74c
liveangela <cs198500@126.com>
2018-08-12 17:33:35 +0800

    [^]Frame: use pug for template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
-<template>
-  <v-app>
-    <v-navigation-drawer
+<template lang="pug">
+  v-app
+    v-navigation-drawer(
       persistent
       :mini-variant="miniVariant"
       :clipped="clipped"
@@ -8,62 +8,45 @@
       enable-resize-watcher
       fixed
       app
-    >
-      <v-list>
-        <v-list-tile
+    )
+      v-list
+        v-list-tile(
           value="true"
           v-for="(item, i) in items"
           :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar app :clipped-left="clipped">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-    <v-navigation-drawer
+        )
+            v-list-tile-action
+              v-icon(v-html="item.icon")
+            v-list-tile-content
+              v-list-tile-title(v-text="item.title")
+    v-toolbar(app :clipped-left="clipped")
+      v-toolbar-side-icon(@click.stop="drawer = !drawer")
+      v-btn(icon @click.stop="miniVariant = !miniVariant")
+        v-icon(v-html="miniVariant ? 'chevron_right' : 'chevron_left'")
+      v-btn(icon @click.stop="clipped = !clipped")
+        v-icon web
+      v-btn(icon @click.stop="fixed = !fixed")
+        v-icon remove
+      v-toolbar-title(v-text="title")
+      v-spacer
+      v-btn(icon @click.stop="rightDrawer = !rightDrawer")
+        v-icon menu
+    v-content
+      HelloWorld
+    v-navigation-drawer(
       temporary
       :right="right"
       v-model="rightDrawer"
       fixed
       app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2018</span>
-    </v-footer>
-  </v-app>
+    )
+      v-list
+        v-list-tile(@click="right = !right")
+          v-list-tile-action
+            v-icon compare_arrows
+          v-list-tile-title Switch drawer (click me)
+    v-footer(:fixed="fixed" app)
+      span &copy; 2018
 </template>
 
 <script lang="ts">
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,14 @@
-<template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+<template lang="pug">
+  v-container(fluid)
+    v-slide-y-transition(mode="out-in")
+      v-layout(column align-center)
+        img.mb-5(src="@/assets/logo.png" alt="Vuetify.js")
+        blockquote
+          | &#8220;First, solve the problem. Then, write the code.&#8221;
+          footer
+            small
+              em
+                | &mdash;John Johnson
 </template>
 
 <script lang="ts">
@@ -25,7 +20,6 @@ export default class HelloWorld extends Vue {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
 h1, h2
   font-weight normal
