769e150051c8db826b862a6bee74797a450afaf1
liveangela <cs198500@126.com>
2018-08-25 09:35:58 +0800

    [^]Game: use Prmise wrapper

--- src/games/index.ts
+++ src/games/index.ts
@@ -1,17 +1,26 @@
 import {IBaseRoleInput, BaseRole} from './BaseRole'
 
+const engine: {[index: string]: any} = {}
 const map: {
   [index: string]: any,
 } = {
   createRole(data: IBaseRoleInput) {
+    return new BaseRole(data)
+  },
+}
+
+// wrap all func in Promise
+Object.keys(map).forEach((funcName) => {
+  const funcBody = map[funcName]
+  engine[funcName] = (data: any) => {
     return new Promise((resolve, reject) => {
       try {
-        resolve(new BaseRole(data))
+        resolve(funcBody(data))
       } catch (e) {
         reject(e)
       }
     })
-  },
-}
+  }
+})
 
-export default map
+export default engine
